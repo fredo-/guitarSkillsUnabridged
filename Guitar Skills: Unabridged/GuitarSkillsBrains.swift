35d46472c2c04e65ccbcd2f391aefc7c63be7a70
Alfredo J. Velasco <velasco622@gmail.com>
2015-11-08 01:19:33 -0500

    Initial commit

---
+++ Guitar Skills: Unabridged/GuitarSkillsBrains.swift
@@ -0,0 +1,20 @@
+//
+//  File.swift
+//  Guitar Skills: Unabridged
+//
+//  Created by Alfredo J. Velasco on 11/8/15.
+//  Copyright © 2015 Wolf Picks. All rights reserved.
+//
+
+import Foundation
+
+class GuitarSkillsBrains{
+    class HistoryList{
+    
+    
+    }
+    
+    var hlist=HistoryList();
+    
+    
+}
