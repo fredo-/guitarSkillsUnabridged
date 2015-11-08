de1a4704977e505fcb9badff285f178f260529ce
Alfredo J. Velasco <velasco622@gmail.com>
2015-11-08 00:11:26 -0500

    Initial Commit

---
+++ Guitar Skills: UnabridgedTests/Guitar_Skills__UnabridgedTests.swift
@@ -0,0 +1,36 @@
+//
+//  Guitar_Skills__UnabridgedTests.swift
+//  Guitar Skills: UnabridgedTests
+//
+//  Created by Alfredo J. Velasco on 11/8/15.
+//  Copyright © 2015 Wolf Picks. All rights reserved.
+//
+
+import XCTest
+@testable import Guitar_Skills__Unabridged
+
+class Guitar_Skills__UnabridgedTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
