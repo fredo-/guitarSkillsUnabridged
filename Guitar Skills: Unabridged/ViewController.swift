ebe6dea2b798d8a2a15419dbc7649e5379484f39
Alfredo J. Velasco <velasco622@gmail.com>
2015-11-08 07:31:01 -0500

    Got database connected

--- Guitar Skills: Unabridged/ViewController.swift
+++ Guitar Skills: Unabridged/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 class ViewController: UIViewController {
 
@@ -14,12 +15,27 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    //---------------------THIS FUNCTION STARTS THE INITIALIZATION OF THE MODEL------------------------
+    @IBAction func makeModel(sender: AnyObject) {
+        Parse.setApplicationId("KWg6vkp0BCIux6pQpftK9oc1M2q6wltAxT7xdba7", clientKey: "3UYovk71VCPwYaiVtH4M1TfE97rcPogwA13D9LUn")
+        //Query database to get the user info
+        let usrInfo = PFQuery(className: "users_table")
+        usrInfo.getObjectInBackgroundWithId("Uyj1gkFx9j") {
+            (gameScore: PFObject?, error: NSError?) -> Void in
+            if error == nil && gameScore != nil {
+                print(gameScore)
+            } else {
+                print(error)
+            }
+        }
+    }
+    //Properties:
+    @IBOutlet weak var userName: UITextField!
+    
 }
 
